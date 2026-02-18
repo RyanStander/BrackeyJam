0c7e7340d70326c02d4222e617b9b69acd560c47
GabesCodes <gabrielkrivenkoff@gmail.com>
2026-02-17 20:42:54 -0800

    added randomized node flashing on intervals that can be configured, reimplemented nodes to match artist designs, commented code heavily since it was a bit complicated, implemented dozens of 'checks' that prevent various methods of cheating.

--- Assets/Scripts/MiniGameSystem/MiniGame_Wiring/WiringMinigame.cs
+++ Assets/Scripts/MiniGameSystem/MiniGame_Wiring/WiringMinigame.cs
@@ -3,27 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// going to spam comments here because this is like the most complicated thing i've made
+/// - each node needs to know its color and whether it's on the left or right side
+/// - when you click a node, it should spawn a line and follow the mouse until you release it
+/// - if you release it over a node of the opposite side and same color, it should snap to that node and mark both as connected
+/// - if you release it anywhere else, it should just disappear and reset the state so you can try again
+/// - once all nodes are connected, it should trigger the win condition
+/// </summary>
+
 public class WiringMinigame : BaseMinigame
 {
     [Header("configuration")]
-    public int wireCount = 6; 
+    public int wireCount = 6;
     public List<Color> AvailableColors;
 
     [Header("references")]
-    public GameObject WirePrefab;    
-    public WiringNode NodePrefab;    
-    public Transform WireParent;     
-    public Transform LeftContainer;  
-    public Transform RightContainer; 
+    public GameObject WirePrefab;
+    public WiringNode NodePrefab;
+    public Transform WireParent;
+    public Transform LeftContainer;
+    public Transform RightContainer;
 
     private WiringNode _currentStartNode;
     private RectTransform _currentWire;
     private int _matchesMade = 0;
 
+    // track the node we are currently hovering over
+    private WiringNode _currentHoveredNode;
+
     public override void StartMinigame()
     {
         base.StartMinigame();
-        
+
         _matchesMade = 0;
         _currentStartNode = null;
         _currentWire = null;
@@ -32,11 +44,13 @@ public class WiringMinigame : BaseMinigame
         for (int i = 0; i < AvailableColors.Count; i++) allColorIndices.Add(i);
 
         ShuffleList(allColorIndices);
+        //grab all colors from our list we defined in inspector, then shuffle it
 
         int count = Mathf.Min(wireCount, AvailableColors.Count);
         List<int> selectedColors = allColorIndices.GetRange(0, count);
 
-        for (int i = 0; i < count; i++)
+        //based on the amount we defined, spawn nodes on left side with colors from our shuffled list
+        for (int i = 0; i < count; i++) 
         {
             int id = selectedColors[i];
             WiringNode node = Instantiate(NodePrefab, LeftContainer);
@@ -46,20 +60,22 @@ public class WiringMinigame : BaseMinigame
 
         ShuffleList(selectedColors);
 
+        //based on the amount we defined, spawn nodes on right side with colors from our shuffled list
+        // we also shuffle again to make sure both sides are different order than eachother.
         for (int i = 0; i < count; i++)
         {
             int id = selectedColors[i];
             WiringNode node = Instantiate(NodePrefab, RightContainer);
             node.Setup(this, id, AvailableColors[id]);
-            node.IsLeftSide = false; //kind of hacky but it works
-            node.transform.localScale = new Vector3(-1, 1, 1); 
+            node.IsLeftSide = false;
+            node.transform.localScale = new Vector3(-1, 1, 1);
         }
     }
 
-
-    public void AttemptConnectionStart(WiringNode node)
+    //called during onpointerdown from WiringNode, spawn a wire, grab it's node color
+    public void AttemptConnectionStart(WiringNode node) 
     {
-        if (_currentStartNode != null) return; 
+        if (_currentStartNode != null) return;
 
         _currentStartNode = node;
 
@@ -67,17 +83,16 @@ public class WiringMinigame : BaseMinigame
         _currentWire = newLine.GetComponent<RectTransform>();
         _currentWire.position = node.transform.position;
         newLine.GetComponent<Image>().color = AvailableColors[node.ColorID];
-        //Debug.Log(newLine.name + " created at " + _currentWire.position);
+
+        newLine.GetComponent<Image>().raycastTarget = false;
     }
 
+    //called on onpointer up from WiringNode, make sure it's a valid node and the colorID's match, then finish connect
     public void AttemptConnectionEnd(WiringNode endNode)
     {
-        if (_currentStartNode == null) 
-            return;
-        if (_currentStartNode == endNode) 
-            return;
-        if (_currentStartNode.IsLeftSide == endNode.IsLeftSide) 
-            return;
+        if (_currentStartNode == null) return;
+        if (_currentStartNode == endNode) return;
+        if (_currentStartNode.IsLeftSide == endNode.IsLeftSide) return;
 
         if (_currentStartNode.ColorID == endNode.ColorID)
         {
@@ -88,18 +103,17 @@ public class WiringMinigame : BaseMinigame
     private void FinishConnection(WiringNode endNode)
     {
         UpdateLineVisual(_currentStartNode.transform.position, endNode.transform.position);
-        Debug.Log("Connection made between " + _currentStartNode.name + " and " + endNode.name);
 
-        _currentStartNode.IsConnected = true;
-        endNode.IsConnected = true;
+        _currentStartNode.SetConnected();
+        endNode.SetConnected();
 
         _currentStartNode = null;
         _currentWire = null;
-
         _matchesMade++;
+        
 
         if (_matchesMade >= Mathf.Min(wireCount, AvailableColors.Count))
-        {       
+        {
             StartCoroutine(WinSequence());
         }
     }
@@ -108,9 +122,16 @@ public class WiringMinigame : BaseMinigame
     {
         if (_currentStartNode != null && _currentWire != null)
         {
-            Destroy(_currentWire.gameObject);
-            _currentStartNode = null;
-            _currentWire = null;
+            if (_currentHoveredNode != null)
+            {
+                AttemptConnectionEnd(_currentHoveredNode);
+            }
+            if (_currentStartNode != null)
+            {
+                Destroy(_currentWire.gameObject);
+                _currentStartNode = null;
+                _currentWire = null;
+            }
         }
     }
 
@@ -119,37 +140,38 @@ public class WiringMinigame : BaseMinigame
         if (_currentStartNode != null && _currentWire != null)
         {
             UpdateLineVisual(_currentStartNode.transform.position, Input.mousePosition);
-
-            if (Input.GetMouseButtonUp(0))
-            {  
-                EndDrag();
-            }  
         }
     }
 
+    public void SetHoveredNode(WiringNode node)
+    {
+        _currentHoveredNode = node;
+    }
+
+    //i'm not gonna lie this was from a tutorial but it works so who cares, calculates stretchy lines from two points
     private void UpdateLineVisual(Vector3 startPos, Vector3 endPos)
     {
         Vector3 direction = endPos - startPos;
         float distance = direction.magnitude;
-        _currentWire.sizeDelta = new Vector2(distance, 15f); 
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; //thank god for tutorials
+        _currentWire.sizeDelta = new Vector2(distance, 15f);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         _currentWire.rotation = Quaternion.Euler(0, 0, angle);
     }
 
+    //fisher-yates shuffle, also from a tutorial
     private void ShuffleList(List<int> list)
     {
         for (int i = 0; i < list.Count; i++)
         {
             int temp = list[i];
             int rnd = Random.Range(i, list.Count);
-            list[i] = list[rnd]; //thank god for tutorials x2
+            list[i] = list[rnd];
             list[rnd] = temp;
         }
     }
     IEnumerator WinSequence()
     {
         yield return new WaitForSeconds(2f);
-        Debug.Log("delay done, closing minigame");
         FinishGame(true);
     }
 }
--- Assets/Scripts/MiniGameSystem/MiniGame_Wiring/WiringNode.cs
+++ Assets/Scripts/MiniGameSystem/MiniGame_Wiring/WiringNode.cs
@@ -1,7 +1,13 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+// / <summary>
+// / each node is a connection point for a wire. 
+// / we begin by setting nodes to grey, then begin couroutines on each one to randomly flicker their color
+// / when you click a node, it should stop flickering and show its real color, then spawn a wire that follows the mouse
+// / </summary>
 public class WiringNode : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler
 {
     [Header("State")]
@@ -10,37 +16,83 @@ public class WiringNode : MonoBehaviour, IPointerDownHandler, IPointerUpHandler,
     public bool IsConnected;
 
     private WiringMinigame _wiregame;
-    private Image _wireNodeImage;
+    public Image PupilImage;
+    //private Image PupilImage;
 
-    private void Awake()
-    {
-        _wireNodeImage = GetComponent<Image>();
-    }
+    public float FlickerInterval = 3.0f;
+    public float FlickerDuration = 0.5f;
+
+    private Color _realColor;
+    private Coroutine _flickerCoroutine;
 
     public void Setup(WiringMinigame wiregame, int id, Color color)
     {
         _wiregame = wiregame;
         ColorID = id;
+        _realColor = color;
         IsConnected = false;
 
-        if (_wireNodeImage == null) _wireNodeImage = GetComponent<Image>();
-        _wireNodeImage.color = color;
+        if (PupilImage == null) 
+            PupilImage = GetComponent<Image>();
+
+        PupilImage.color = Color.white;
+
+        if (_flickerCoroutine != null) 
+            StopCoroutine(_flickerCoroutine);
+
+        _flickerCoroutine = StartCoroutine(FlickerRoutine());
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    IEnumerator FlickerRoutine()
     {
-        if (!IsConnected) _wiregame.AttemptConnectionStart(this);
-        //Debug.Log("Clicked on node with color ID: " + ColorID);
+        yield return new WaitForSeconds(Random.Range(0f, 3f));
+
+        while (!IsConnected)
+        {
+            PupilImage.color = _realColor;
+            yield return new WaitForSeconds(FlickerDuration);
+
+            if (IsConnected) break;
+
+            PupilImage.color = Color.white;
+            yield return new WaitForSeconds(FlickerInterval + Random.Range(0f, 3f));
+        }
+        PupilImage.color = _realColor;
+    }
+
+    //a bit hacky but I wanted to make sure the node color is set right once you connect it
+    public void SetConnected()
+    {
+        IsConnected = true;
+        if (_flickerCoroutine != null) StopCoroutine(_flickerCoroutine);
+        PupilImage.color = _realColor;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (!IsConnected) _wiregame.AttemptConnectionEnd(this);
+        if (!IsConnected) _wiregame.SetHoveredNode(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _wiregame.SetHoveredNode(null);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        if (!IsConnected)
+        {
+            PupilImage.color = Color.white; //wanted it to immediately turn to white when released to stop cheating
+        }
         _wiregame.EndDrag();
-        //Debug.Log("Released node with color ID: " + ColorID);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!IsConnected)
+        {
+            _wiregame.AttemptConnectionStart(this);
+            PupilImage.color = _realColor;
+        }
     }
 }
