69e90bdfff9fe6b8fb02f09cf6152ad1ea014631
Ryan <stander.ryan@gmail.com>
2026-02-20 21:32:34 +0100

    Room traveling

--- Assets/Scripts/InteractionSystem/InteractRoom.cs
+++ Assets/Scripts/InteractionSystem/InteractRoom.cs
@@ -1,19 +1,49 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using UI;
 using UnityEngine;
 
-public class InteractRoom : MonoBehaviour, IInteractable
+namespace InteractionSystem
 {
-    public Transform roomEntryPoint;
-    public bool Interact(Interaction Interaction)
+    public class InteractRoom : MonoBehaviour, IInteractable
     {
-        Debug.Log("Player is entering room");
-        MovePlayerToRoom(Interaction.gameObject);
-        return true;
-    }
+        public Transform roomEntryPoint;
+        [SerializeField] private GameObject[] _objectsToActivate;
+        [SerializeField] private GameObject[] _objectsToDeactivate;
+        [SerializeField] private FadeToBlack _fadeToBlack;
+        private Coroutine _movePlayerCoroutine;
 
-    public void MovePlayerToRoom(GameObject player)
-    {
-        player.transform.position = roomEntryPoint.position;
+        private void OnValidate()
+        {
+            if (_fadeToBlack == null)
+                _fadeToBlack = FindObjectOfType<FadeToBlack>();
+        }
+
+        public bool Interact(Interaction interaction)
+        {
+            _movePlayerCoroutine =StartCoroutine(MovePlayerToRoom(interaction.gameObject));
+        
+            return true;
+        }
+
+        private IEnumerator MovePlayerToRoom(GameObject player)
+        {
+                _fadeToBlack.StartFadeToBlack();
+                while (_fadeToBlack.IsFading())
+                {
+                    yield return null;
+                }
+                foreach (GameObject activateObject in _objectsToActivate)
+                {
+                    activateObject.SetActive(true);
+                }
+        
+                foreach (GameObject deactivateObject in _objectsToDeactivate)
+                {
+                    deactivateObject.SetActive(false);
+                }
+                
+                player.transform.position = roomEntryPoint.position;
+        }
     }
 }
