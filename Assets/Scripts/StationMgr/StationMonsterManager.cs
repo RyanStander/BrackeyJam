2c477cc9907ed18fdb80ff0f2ec15bc7e57021de
Ryan <stander.ryan@gmail.com>
2026-02-21 17:40:53 +0100

    FinishedMosnterLevel

--- Assets/Scripts/StationMgr/StationMonsterManager.cs
+++ Assets/Scripts/StationMgr/StationMonsterManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using AudioManagement;
 using PersistentManager;
+using UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace StationMgr
 {
@@ -13,6 +15,9 @@ namespace StationMgr
         [Header("First Wire Completion Actions")] [SerializeField]
         private Animator _tentacleMonsterAnimator;
 
+        [SerializeField] private GameObject[] _firstWireObjectsToDisable;
+        [SerializeField] private GameObject[] _firstWireObjectsToEnable;
+
         [SerializeField] private GameObject _largeTentacleMonster;
 
         #endregion
@@ -22,9 +27,23 @@ namespace StationMgr
         [Header("Second Wire Completion Actions")] [SerializeField]
         private Animator _tentacleDoorAnimator;
 
+        [SerializeField] private GameObject[] _secondWireObjectsToDisable;
+        [SerializeField] private GameObject[] _secondWireObjectsToEnable;
+
         [SerializeField] private GameObject _tentacleDoorInteractable;
 
         #endregion
+
+        #region Third wire actions
+
+        [Header("Third Wire Completion Actions")] [SerializeField]
+        private GameObject[] _thirdWireObjectsToDisable;
+
+        [SerializeField] private GameObject[] _thirdWireObjectsToEnable;
+
+        #endregion
+        
+        [SerializeField] private Animator _rewardAnimator;
 
         //not sure what onobjectiveupdate does, but keeping this here for later
         /*public void CompleteWireTask()
@@ -37,14 +56,25 @@ namespace StationMgr
         public void FirstWireFixed()
         {
             _tentacleMonsterAnimator.Play("MonsterUpTentacleLeave");
+            foreach (GameObject obj in _firstWireObjectsToDisable)
+            {
+                obj.SetActive(false);
+            }
+
+            foreach (GameObject obj in _firstWireObjectsToEnable)
+            {
+                obj.SetActive(true);
+            }
+
             AudioManager.Play(AudioDataHandler.StationMonster.Growl());
-                StartCoroutine(WaitForTentacleToLeave());
+            StartCoroutine(WaitForTentacleToLeave());
         }
 
         private IEnumerator WaitForTentacleToLeave()
         {
             //wait until the animation is New State
-            yield return new WaitUntil(() => _tentacleMonsterAnimator.GetCurrentAnimatorStateInfo(0).IsName("New State"));
+            yield return new WaitUntil(
+                () => _tentacleMonsterAnimator.GetCurrentAnimatorStateInfo(0).IsName("New State"));
             _largeTentacleMonster.SetActive(false);
         }
 
@@ -52,12 +82,54 @@ namespace StationMgr
         {
             _tentacleDoorAnimator.Play("TentacleOpen");
             _tentacleDoorInteractable.SetActive(true);
+            foreach (GameObject obj in _secondWireObjectsToDisable)
+            {
+                obj.SetActive(false);
+            }
+
+            foreach (GameObject obj in _secondWireObjectsToEnable)
+            {
+                obj.SetActive(true);
+            }
+
             AudioManager.Play(AudioDataHandler.StationMonster.Growl());
         }
 
 
         public void ThirdWireFixed()
         {
+            foreach (GameObject obj in _thirdWireObjectsToDisable)
+            {
+                obj.SetActive(false);
+            }
+
+            foreach (GameObject obj in _thirdWireObjectsToEnable)
+            {
+                obj.SetActive(true);
+            }
+
+            AudioManager.Play(AudioDataHandler.StationMonster.Growl());
+        }
+
+        public void PlayerTookReward()
+        {
+            TrainDataHandler.Refuel(1);
+            _rewardAnimator.Play("RewardTentacleLeave");
+        }
+
+        public void ReturnToSpace()
+        {
+            StartCoroutine(FadeToLoad());
+        }
+
+        private IEnumerator FadeToLoad()
+        {
+            FadeToBlack fadeToBlack = FindObjectOfType<FadeToBlack>();
+            fadeToBlack.StartFadeToBlack();
+
+            yield return new WaitUntil(() => !fadeToBlack.IsFading());
+            
+            SceneManager.LoadScene("TrainTravelScene");
         }
 
         protected override void CheckObjectives()
